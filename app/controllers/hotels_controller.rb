901e52a6ed1ab2a734d95d42dfd0505400ef126c
Marion Rigard Cerison <marion.rigard@gmail.com>
2022-03-23 13:09:56 +0100

    controller hotel et routes

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,29 @@
 class HotelsController < ApplicationController
+  def index
+    @hotels = Hotel.all
+  end
+
+  def show
+    @hotel = Hotel.find(params[:id])
+  end
+
+  def new
+    @hotel = Hotel.new
+  end
+
+  def create
+    @hotel = Hotel.new(hotel_params)
+    if @hotel.save
+      redirect_to hotel_path(@hotel)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def hotel_params
+    params.require(:hotel).permit(:name, :address, :country, :city)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :hotels
-  resources :bookings
+  root to: 'hotels#show'
+
+  resources :hotels do
+    resources :rooms, only: [:index]
+  end
 end
